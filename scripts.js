75c9853655bb28a1798a8dbd789f91808b742e26
Samuel Pires <samueljackbill@gmail.com>
2025-02-18 21:15:26 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function functionSoma() {
+	var a = Math.round(prompt("Informe o valor de a: ", 10));
+	var b = Math.round(prompt("Informe o valor de b: ", 5));
+	var soma = a+b;
+	
+	alert("O valor de a + b é: " + soma);
+}
